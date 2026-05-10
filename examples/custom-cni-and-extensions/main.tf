4a9f014b3c894205497645ce825047309cb5c530
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-10 17:55:22 +0200

    add custom-cni-and-extensions example

---
+++ examples/custom-cni-and-extensions/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "~> 1.15"
+
+  required_providers {
+    talos = {
+      source  = "siderolabs/talos"
+      version = "~> 0.11.0"
+    }
+  }
+}
+
+provider "talos" {}
+
+module "talos_cluster" {
+  source = "../../"
+
+  control_plane_ips = var.control_plane_ips
+  worker_ips        = var.worker_ips
+  talos_version     = var.talos_version
+  cluster_name      = var.cluster_name
+  cluster_endpoint  = var.cluster_endpoint
+  node_subnet       = var.node_subnet
+
+  schematic_extensions = var.schematic_extensions
+
+  extra_control_plane_patches = [
+    yamlencode({
+      cluster = {
+        network = { cni = { name = "none" } }
+        proxy   = { disabled = true }
+      }
+    })
+  ]
+}
